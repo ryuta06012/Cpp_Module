8dbb4e036060ca8ac735ca8bf8b2ef1e15f550fd
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-12-03 15:57:06 +0900

    コンストラクタでWeaponを受け取るように変更しました

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -6,13 +6,13 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 16:09:27 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/02 15:31:53 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/12/03 15:45:31 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, std::string weapon): _name(name), _weapon(weapon){}
+HumanA::HumanA(std::string name, Weapon &weapon): _name(name), _weapon(weapon){}
 
 HumanA::~HumanA(){}
 
