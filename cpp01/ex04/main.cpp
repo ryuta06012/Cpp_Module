8715287aaeeaf9d8c81f6aa4ab71af70c1fd130c
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-12-05 05:41:22 +0900

    finish ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 07:32:36 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/04 16:02:46 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/12/05 04:07:02 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,16 @@
 
 int main(int ac, char **av) {
 
-    // pemファイル読み込み
-	if (ac < 2)
+	if (ac != 4)
 	{
-		std::cout << "aaaa" << std::endl;
-		std::cout << "usage:" << std::endl;
-		std::cout << "./replace FILENAME STRING REPLACEMENT" << std::endl;
+		std::cerr << "usage:" << std::endl;
+		std::cerr << "./replace FILENAME STRING REPLACEMENT" << std::endl;
 		return (-1);
 	}
+
 	std::string	file = av[1];
     std::ifstream inFile(file);
-	std::ofstream outFile(file + ".replace");
+	std::ofstream newFile(file + ".replace");
 	std::string	checkStr(av[2]);
 	std::string	replaceStr(av[3]);
     std::string str;
@@ -38,24 +37,26 @@ int main(int ac, char **av) {
         std::cerr << "File Open Error" << std::endl;
         return (-1);
     }
-    while (getline(inFile, line))
+	if (newFile.fail())
 	{
-			std::cout << "line" << std::endl;
-           	std::cout << line  << std::endl;
-			for (int i=0; i<5; i++)
+		std::cerr << "File Open Error" << std::endl;
+        return (-1);
+	}
+    while (std::getline(inFile, line))
+	{
+			for (;;)
 			{
 				pos = line.find(checkStr);
-				std::cout << pos << std::endl;
 				if (pos == std::string::npos)
 					break;
 				else
 				{
+					line.erase(pos, checkStr.length());
 					line.insert(pos, replaceStr);
 				}
 			}
             str += line + "\n";
     }
-    std::cout << str << std::endl;
-
-    return 0;
+    newFile << str;
+    return (0);
 }
