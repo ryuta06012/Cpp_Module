ddef825754c84eb03cc46ae8de165f580a259a99
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-12-06 03:54:27 +0900

    new create file

--- cpp01/ex05/Karen.hpp
+++ cpp01/ex05/Karen.hpp
@@ -6,12 +6,25 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 05:45:02 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/05 05:45:04 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/12/06 03:53:23 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef KAREN_H
 #define KAREN_H
+
+class Karen
+{
+private:
+	void	_debug(void);
+	void	_info(void);
+	void	_warning(void);
+	void	_error(void);
+public:
+	Karen();
+	~Karen();
+	void	complain(std::string level);
+};
 
 
 #endif
