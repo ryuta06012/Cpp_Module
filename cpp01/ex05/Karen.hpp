ee74f59ba01dd557fb3c16ad522903e1dcf4ac67
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-12-11 17:04:48 +0900

    Add a function pointer

--- cpp01/ex05/Karen.hpp
+++ cpp01/ex05/Karen.hpp
@@ -6,12 +6,16 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 05:45:02 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/06 03:53:23 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/12/11 16:51:45 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef KAREN_H
 #define KAREN_H
+
+# include <string>
+# include <iostream>
+# include <fstream>
 
 class Karen
 {
@@ -20,6 +24,8 @@ private:
 	void	_info(void);
 	void	_warning(void);
 	void	_error(void);
+	void	_non(void);
+	void	(Karen::*f[5])(void);
 public:
 	Karen();
 	~Karen();
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,13 +6,20 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 05:45:08 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/05 05:45:27 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/12/11 17:02:49 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
+void	test()
+{
+
+}
+
 int	main()
 {
-	
+	Karen	karen;
+	std::string str;
+	karen.complain(str);
 }
