33e413c2091e38811ceffdb166e96fcdcc9dcecb
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2021-12-12 04:19:41 +0900

    Change pass-by-reference

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,20 +6,31 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 05:45:08 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/11 17:02:49 by hryuuta          ###   ########.fr       */
+/*   Updated: 2021/12/12 04:16:39 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 
-void	test()
+void	test(Karen &karen, std::string str)
 {
-
+	karen.complain(str);
 }
 
 int	main()
 {
 	Karen	karen;
-	std::string str;
-	karen.complain(str);
+	
+	std::cout << "DEBUG" << std::endl;
+	test(karen, "DEBUG");
+	std::cout << "WARNING" << std::endl;
+	test(karen, "WARNING");
+	std::cout << "INFO" << std::endl;
+	test(karen, "INFO");
+	std::cout << "ERROR" << std::endl;
+	test(karen, "ERROR");
+	std::cout << "aaaa" << std::endl;
+	test(karen, "aaaa");
+
+	return (0);
 }
