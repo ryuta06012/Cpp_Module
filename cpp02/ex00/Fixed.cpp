8f3b1b1f6689556468b86153b3aebd0f53b9e78d
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2022-01-18 17:04:33 +0900

    add fixed file

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,28 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 17:02:21 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/12 17:02:23 by hryuuta          ###   ########.fr       */
+/*   Updated: 2022/01/18 15:54:34 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(): _rawBits(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	std::cout << _rawBits << std::endl;
+	//return ;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	std::cout << fixed._rawBits << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
 
--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,7 +6,20 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 17:02:33 by hryuuta           #+#    #+#             */
-/*   Updated: 2021/12/12 17:02:34 by hryuuta          ###   ########.fr       */
+/*   Updated: 2022/01/18 15:53:23 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Fixed.hpp"
+int main( void )
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	//std::cout << a.getRawBits() << std::endl;
+	//std::cout << b.getRawBits() << std::endl;
+	//std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
