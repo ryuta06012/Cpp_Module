00e736173882595d80894e21a5ad1bc60aaff928
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2022-01-23 16:18:13 +0900

    create oerator

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 17:02:21 by hryuuta           #+#    #+#             */
-/*   Updated: 2022/01/18 15:54:34 by hryuuta          ###   ########.fr       */
+/*   Updated: 2022/01/23 15:44:46 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,37 @@
 Fixed::Fixed(): _rawBits(0)
 {
 	std::cout << "Default constructor called" << std::endl;
-	std::cout << _rawBits << std::endl;
-	//return ;
+	//std::cout << _rawBits << std::endl;
 }
 
 Fixed::Fixed(const Fixed &fixed)
 {
 	std::cout << "Copy constructor called" << std::endl;
-	std::cout << fixed._rawBits << std::endl;
+	_rawBits = fixed.getRawBits();
+	//std::cout << fixed._rawBits << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	_rawBits = fixed.getRawBits();
+	return (*this);
 }
 
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	_rawBits = raw;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_rawBits);
 }
 
