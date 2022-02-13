dec8d20a408fb3bb290fa08d79df90f4d2b01f3a
半田隆太 <handaryuuta@handaryuutanoMacBook-Air.local>
2022-02-13 13:49:56 +0900

    create ex00

---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/12 14:24:24 by hryuuta           #+#    #+#             */
+/*   Updated: 2022/02/13 12:46:05 by hryuuta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <exception>
+#include <ostream>
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		int _grade;
+		const int _max_grade;
+		const int _min_grade;
+		const std::string _name;
+		class GradeTooHighException: public std::out_of_range
+		{
+			public:
+				GradeTooHighException(const std::string errStr);
+		};
+		class GradeTooLowException : public std::out_of_range
+		{
+			public:
+				GradeTooLowException(const std::string errStr);
+   		};
+	public:
+		Bureaucrat();
+		Bureaucrat(const std::string name, int num);
+		~Bureaucrat();
+		std::string getName() const;
+		int getGrade() const;
+		void decGrade();
+		void incGrade();
+};
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &b);
+
+#endif
--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,48 @@
 /*   By: hryuuta <hryuuta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 14:00:19 by hryuuta           #+#    #+#             */
-/*   Updated: 2022/02/12 14:00:20 by hryuuta          ###   ########.fr       */
+/*   Updated: 2022/02/13 12:46:25 by hryuuta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+int main() {
+	try
+	{
+		Bureaucrat a("a", 20);
+		std::cout << a << std::endl;
+		a.decGrade();
+		std::cout << a << std::endl;
+	}
+	catch(std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat a("a", 151);
+		std::cout << a << std::endl;
+		a.decGrade();
+		std::cout << a << std::endl;
+	}
+	catch(std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat a("a", 3);
+		std::cout << a << std::endl;
+		a.incGrade();
+		std::cout << a << std::endl;
+		a.incGrade();
+		std::cout << a << std::endl;
+		a.incGrade();
+		std::cout << a << std::endl;
+	}
+	catch(std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
